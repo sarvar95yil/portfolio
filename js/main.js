0f58d8c1f163e791bf0fcc5687e541da0ab1d3ad
bartsimpson <sarvar95yil@gmail.com>
2020-08-21 13:08:43 +0500

    First Site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true,
+        
+        
+    },
+});
